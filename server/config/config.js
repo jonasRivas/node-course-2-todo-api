d422bf61658f65cdceb0ebf039332197a61fd83b
Jonas <jard.dugarte@gmail.com>
2017-11-21 15:37:47 -0600

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,21 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key)=>{
+    process.env[key] = envConfig[key];
+  });
 }
-else if (env === 'production') {
-  process.env.MONGODB_URI = 'mongodb://root:root@ds163705.mlab.com:63705/node-todo-api';
-}
+
+// if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
+// else if (env === 'production') {
+//   process.env.MONGODB_URI = 'mongodb://root:root@ds163705.mlab.com:63705/node-todo-api';
+// }
